82927574553476b773fc70436cde9e64d3a339f3
Jannes Effendi <55879025+DJannes@users.noreply.github.com>
2022-01-02 18:42:35 +0700

    [feature] Update Topic posts when create new post

--- src/main/java/com/project/app/Note.md
+++ src/main/java/com/project/app/Note.md
@@ -2,4 +2,5 @@ Alur
 
 Fitur yang di buat : 
 1. GetAllPost pada Topic
-2. 
+2. Setiap Create Post, tambahkan post pada Topic => Done
+3. 
--- src/main/java/com/project/app/service/impl/PostServiceImpl.java
+++ src/main/java/com/project/app/service/impl/PostServiceImpl.java
@@ -1,12 +1,16 @@
 package com.project.app.service.impl;
 
 import com.project.app.entity.Post;
+import com.project.app.entity.Topic;
 import com.project.app.exception.NotFoundException;
 import com.project.app.repository.PostRepository;
+import com.project.app.repository.TopicRepository;
 import com.project.app.service.PostService;
+import com.project.app.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +18,9 @@ import java.util.Optional;
 public class PostServiceImpl implements PostService {
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private TopicService topicService;
 
     @Override
     public Post getById(String id) {
@@ -31,8 +38,17 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional
     public Post create(Post post) {
-        return postRepository.save(post);
+        Post savedPost = postRepository.save(post);
+
+        Topic topic = topicService.getById(savedPost.getTopic().getId());
+        List<Post> posts = topic.getPosts();
+        posts.add(savedPost);
+        topic.setPosts(posts);
+        topicService.create(topic);
+
+        return savedPost;
     }
 
     @Override
