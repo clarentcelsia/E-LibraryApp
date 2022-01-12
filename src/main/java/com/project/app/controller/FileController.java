05d335c03955a03d2fcf97d8d4ee3648bff58667
Clarenti Celsia <clarentic@gmail.com>
2022-01-11 22:00:38 -0800

    [Improve] merge loan

--- src/main/java/com/project/app/controller/FileController.java
+++ src/main/java/com/project/app/controller/FileController.java
@@ -1,44 +1,32 @@
 package com.project.app.controller;
 
-import com.project.app.files.Files;
-import com.project.app.response.Response;
-import com.project.app.service.FileService;
+import com.project.app.entity.library.Files;
+import com.project.app.service.impl.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import static com.project.app.util.Utility.RESPONSE_GET_SUCCESS;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/files")
-public class FileController {
-
+public class FIleController {
     @Autowired
-    FileService fileService;
+    private FileService fileService;
+
 
     @GetMapping("/{id}")
-    public ResponseEntity<byte[]> getFile(
-            @PathVariable String id
-    ) {
-        Files file = fileService.getFile(id);
+    public ResponseEntity<byte[]> getFile(@PathVariable String id) {
+        Files file = fileService.getFileById(id);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .header(HttpHeaders.CONTENT_DISPOSITION,
                         "attachment; filename=\""
-                                + file.getFilename() + "\"") //img.png
+                                + file.getFileName() + "\"")
                 .body(file.getData());
-    }
-
-    @GetMapping("/info/{id}")
-    public ResponseEntity<Response<Files>> getFileInfo(
-            @PathVariable String id
-    ) {
-        Files file = fileService.getDataFileById(id);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(new Response<>(RESPONSE_GET_SUCCESS, file));
     }
 }
